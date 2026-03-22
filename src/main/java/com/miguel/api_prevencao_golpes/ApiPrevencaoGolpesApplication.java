6b7c6eb72de9a8da19fcb269a78b9ce94acc6bf9
MiguelRebequi <130229587+MiguelRebequi@users.noreply.github.com>
2026-03-22 17:51:32 -0300

    Instalando o Springboot com as dependências pelo Spring initializr

---
+++ src/main/java/com/miguel/api_prevencao_golpes/ApiPrevencaoGolpesApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.api_prevencao_golpes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPrevencaoGolpesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPrevencaoGolpesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/api_prevencao_golpes/ApiPrevencaoGolpesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.api_prevencao_golpes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPrevencaoGolpesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
