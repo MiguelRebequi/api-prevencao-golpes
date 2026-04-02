370620b7509e36171dd3b9941ecaf8a245b58052
MiguelRebequi <130229587+MiguelRebequi@users.noreply.github.com>
2026-04-01 22:49:04 -0300

    feat: cria UsuarioController, UsuarioService e configura banco H2(Temporariamente antes de um banco de dados real)

--- src/main/java/com/miguel/api_prevencao_golpes/model/Usuario.java
+++ src/main/java/com/miguel/api_prevencao_golpes/model/Usuario.java
@@ -7,17 +7,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "tabela_usuarios")
 public class Usuario {
+
     // Criarei os primeiros atributos do projeto, no momento serão apenas esses os mais importante, o MVP seria esse:
     // ---
     // Primeiramente, criaremos em ID, caso a professora ou tiver tempo para aprender o UUID seja uma ideia implementada no futuro.
     // Dúvida: Long ou long? Como havia sentido, criar inicialmente como Long para não precisar no construtor inserir o ID e ser feito depois em um método incremental.
     // Para colocar como padrão, colocarei o ID na primeira linha dos atributos por ser Identificador único.
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "nome_usuario", nullable = false)
     private String nomeUsuario;
@@ -31,37 +31,59 @@ public class Usuario {
     // Por padrão, permite nulo, mas deixarei para poder inverter, caso ache melhor.
     @Column(nullable = true, unique = true)
     private String cpf;
-    
+
+    // Criarei um construtor vazio e um construtor com atributos(Mockados?)
+    // Esse serve para a Spring
+    public Usuario() {
+    }
+
+    // Esse serve para testar e fazer o Postman, por exemplo.
+        // Observa que eu criei sem o ID por ser incremental pelo Atomic em UsuarioService.
+    public Usuario(String nomeUsuario, String senha, String email, String cpf) {
+        this.nomeUsuario = nomeUsuario;
+        this.senha = senha;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNomeUsuario() {
         return nomeUsuario;
     }
+
     public void setNomeUsuario(String nomeUsuario) {
         this.nomeUsuario = nomeUsuario;
     }
+
     public String getSenha() {
         return senha;
     }
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
-    
 }
---
+++ src/main/java/com/miguel/api_prevencao_golpes/service/UsuarioService.java
@@ -0,0 +1,37 @@
+package com.miguel.api_prevencao_golpes.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import com.miguel.api_prevencao_golpes.model.Usuario;
+
+@Service
+public class UsuarioService {
+    // Assim como a professora inicialmente, eu farei uma lista ao invés do banco de dados temporariamente
+    private static final List<Usuario> usuarios = new ArrayList<>();
+    // Precisamos criar um contador de ID, a professora usou o Atomic, segundo minhas pesquisas, essa classe permite a inserção sem ter a possibilidade de dois ID's iguais, por exemplo.
+    private static final AtomicLong contador = new AtomicLong();
+
+    public List<Usuario> findAll(){
+        return usuarios;
+    }
+
+    // Esse optional serve para evitar o NPE
+    public Optional<Usuario> findUsuariobyID(Long id){
+        // Stream é uma esteira semelhante ao for imperativo, mas definitivamente mais fácil e limpo de escrever o código
+        // Obs: Igualmente ou um pouco mais lento, mas ainda assim pode valer em alguns casos.
+            // Esse stream filtra o usuário comparando todos os ID com o ID buscado e entrega o primeiro valor
+            // Caso não encontrar, o Optional deve um Vazio sem dar erro de NPE
+        return usuarios.stream().filter(usuario -> usuario.getId().equals(id)).findFirst();
+    }
+
+    public Usuario save(Usuario usuario){
+        usuario.setId(contador.incrementAndGet());
+        usuarios.add(usuario);
+        return usuario;
+    }
+}
